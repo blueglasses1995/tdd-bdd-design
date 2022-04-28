f048d0a3f48e2f6f398d326ef235cd3a1b721fcf
blueglasses1995 <36224897+blueglasses1995@users.noreply.github.com>
2022-04-29 00:50:40 +0700

    [ATM] error test case 22

--- src/bank.ts
+++ src/bank.ts
@@ -2,7 +2,7 @@ import { Account } from './account';
 import { Balance } from './balandce';
 
 export class Bank {
-  private readonly balances: Array<Balance> = [];
+  private readonly balances: Array<Balance> = [new Balance('1', 1000)];
 
   balanceOfAccount(account: Account) {
     const data = this.balances.find(({ accountId }) => {
