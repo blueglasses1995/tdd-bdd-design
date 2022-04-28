d391fd286f835de1656588a53dfbd6d36a1d2fb3
blueglasses1995 <36224897+blueglasses1995@users.noreply.github.com>
2022-04-29 00:44:56 +0700

    [ATM] error test case 20

--- src/accountRegistry.ts
+++ src/accountRegistry.ts
@@ -1,7 +1,10 @@
 import { Account } from './account';
 
 export class AccountRegistry {
+  private readonly accounts: Account[] = [];
+
   findByCustomerId(id: string): Account | null {
-    return null;
+    const account = this.accounts.find((account) => account.id === id);
+    return account || null;
   }
 }
--- src/atm.spec.ts
+++ src/atm.spec.ts
@@ -1,28 +1,30 @@
+import { AccountRegistry } from './accountRegistry';
 import { Atm } from './atm';
+import { Bank } from './bank';
 import { Customer } from './customer';
 
 describe(Atm, () => {
   it('は<Given>特定顧客のバランスが0円の時、<When>特定顧客のバランスを問い合わせると、<Then>バランスが0円返ってくる', () => {
-    const atm = new Atm();
-    const customer = new Customer();
+    const atm = new Atm(new Bank(), new AccountRegistry());
+    const customer = new Customer('');
     expect(atm.balance(customer)).toEqual(0);
   });
 
   it('は<Given>特定顧客のバランスが1000円の時、<When>特定顧客のバランスを問い合わせると、<Then>バランスが1000円返ってくる', () => {
-    const atm = new Atm();
-    const customer = new Customer();
+    const atm = new Atm(new Bank(), new AccountRegistry());
+    const customer = new Customer('');
     expect(atm.balance(customer)).toEqual(1000);
   });
 
   it('は<Given>顧客が存在しない時、<When>その顧客のバランスを問い合わせると、<Then>エラーが返される', () => {
-    const atm = new Atm();
-    const customer = new Customer();
+    const atm = new Atm(new Bank(), new AccountRegistry());
+    const customer = new Customer('');
     expect(atm.balance(customer)).toThrowError();
   });
 
   it('は特定顧客の口座から出金できる', () => {
-    const atm = new Atm();
-    const customer = new Customer();
+    const atm = new Atm(new Bank(), new AccountRegistry());
+    const customer = new Customer('');
     expect(() => atm.withdraw(1000, customer)).not.toThrowError();
   });
 });
