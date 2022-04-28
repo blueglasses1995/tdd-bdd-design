582aef87806516c0324f1367e5b584c07b3f3861
blueglasses1995 <36224897+blueglasses1995@users.noreply.github.com>
2022-04-29 00:31:24 +0700

    [ATM] error test case 18

--- src/atm.ts
+++ src/atm.ts
@@ -1,14 +1,19 @@
 import { AccountRegistry } from './accountRegistry';
+import { Balance } from './balandce';
 import { Bank } from './bank';
 import { Customer } from './customer';
 
 export class Atm {
   constructor(private bank: Bank, private accountRegistry: AccountRegistry) {}
 
-  balance(customer: Customer): number {
+  balance(customer: Customer): Balance {
     const account = this.accountRegistry.findByCustomerId(customer.id);
-    this.bank.balanceOfAccount(account);
-    return 0;
+
+    if (account === null) {
+      throw new Error('Account not found');
+    }
+
+    return this.bank.balanceOfAccount(account);
   }
 
   withdraw(number: number, customer: Customer): void {}
