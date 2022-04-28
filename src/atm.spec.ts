eb55e16b754023efb61204b6522b4baed4f237ed
blueglasses1995 <36224897+blueglasses1995@users.noreply.github.com>
2022-04-29 05:39:05 +0700

    [ATM] success test case 6

--- src/atm.spec.ts
+++ src/atm.spec.ts
@@ -8,31 +8,56 @@ describe(Atm, () => {
   let customer: Customer;
   let fakeBank: Bank;
 
+  // CommonGiven
   beforeEach(() => {
     customer = new Customer('1');
     fakeBank = new Bank();
   });
 
   it('は<Given>特定顧客のバランスが0円の時、<When>特定顧客のバランスを問い合わせると、<Then>バランスが0円返ってくる', () => {
+    // Given
     fakeBank.balances = [new Balance('1', 0)];
     const atm = new Atm(fakeBank, new AccountRegistry());
-    expect(atm.balance(customer)).toEqual(new Balance('1', 0));
+
+    // When
+    const balance = atm.balance(customer);
+
+    // Then
+    expect(balance).toEqual(new Balance('1', 0));
   });
 
   it('は<Given>特定顧客のバランスが1000円の時、<When>特定顧客のバランスを問い合わせると、<Then>バランスが1000円返ってくる', () => {
+    // Given
     fakeBank.balances = [new Balance('1', 1000)];
     const atm = new Atm(fakeBank, new AccountRegistry());
-    expect(atm.balance(customer)).toEqual(new Balance('1', 1000));
+
+    // When
+    const balance = atm.balance(customer);
+
+    // Then
+    expect(balance).toEqual(new Balance('1', 1000));
   });
 
   it('は<Given>顧客が存在しない時、<When>その顧客のバランスを問い合わせると、<Then>エラーが返される', () => {
+    // Given
     const atm = new Atm(new Bank(), new AccountRegistry());
     const customer = new Customer('0');
-    expect(() => atm.balance(customer)).toThrowError();
+
+    // When
+    const getBalance = () => atm.balance(customer);
+
+    // Then
+    expect(getBalance).toThrowError();
   });
 
   it('は特定顧客の口座から出金できる', () => {
+    // Given
     const atm = new Atm(new Bank(), new AccountRegistry());
-    expect(() => atm.withdraw(1000, customer)).not.toThrowError();
+
+    // When
+    const withdrawing = atm.withdraw(1000, customer);
+
+    // Then
+    expect(() => withdrawing).not.toThrowError();
   });
 });
