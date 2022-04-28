0e402d976257c2edd39610a9f0c5fa42e29ea872
blueglasses1995 <36224897+blueglasses1995@users.noreply.github.com>
2022-04-28 22:51:01 +0700

    [ATM] error test case 7

--- src/atm.spec.ts
+++ src/atm.spec.ts
@@ -8,6 +8,12 @@ describe(Atm, () => {
     expect(atm.balance(customer)).toEqual(0);
   });
 
+  it('は<Given>特定顧客のバランスが1000円の時、<When>特定顧客のバランスを問い合わせると、<Then>バランスが1000円返ってくる', () => {
+    const atm = new Atm();
+    const customer = new Customer();
+    expect(atm.balance(customer)).toEqual(1000);
+  });
+
   it('は特定顧客の口座から出金できる', () => {
     const atm = new Atm();
     const customer = new Customer();
