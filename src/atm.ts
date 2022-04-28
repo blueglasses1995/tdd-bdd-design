f8c65d28bc144b6f1ad50bb3eef98e8a2bc50679
blueglasses1995 <36224897+blueglasses1995@users.noreply.github.com>
2022-04-28 22:58:49 +0700

    [ATM] error test case 9

--- src/atm.ts
+++ src/atm.ts
@@ -1,7 +1,11 @@
 import { Customer } from './customer';
 
+class Bank {}
 export class Atm {
+  constructor(private bank: Bank) {}
+
   balance(customer: Customer): number {
+    this.bank.balanceOfCustomer(customer.id);
     return 0;
   }
 
